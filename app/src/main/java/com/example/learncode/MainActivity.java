ef29f145734d09928cdaae873b1baa97c0b38070
Abdulateef Oladele <oladelea@vcu.edu>
2019-01-28 20:33:42 -0500

    Configured SQLite Database // Connected to Main page

--- app/src/main/java/com/example/learncode/MainActivity.java
+++ app/src/main/java/com/example/learncode/MainActivity.java
@@ -7,20 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
     EditText textUsername;
     EditText textPassword;
     Button buttonLogin;
     TextView textViewRegister;
-
+    DatabaseHelper db;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        db = new DatabaseHelper(this);
         textUsername = (EditText)findViewById(R.id.text_username);
         textPassword = (EditText)findViewById(R.id.text_password);
         buttonLogin = (Button)findViewById(R.id.button_login);
@@ -33,5 +36,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(registerIntent);
             }
         });
+            buttonLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String user = textUsername.getText().toString().trim();
+                    String pwd = textPassword.getText().toString().trim();
+                    Boolean res = db.checkUser(user,pwd);
+                    if (res == true)
+                    {
+                        Toast.makeText(MainActivity.this, "Successfully Logged in", Toast. LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "login Error", Toast. LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+
+
     }
 }
