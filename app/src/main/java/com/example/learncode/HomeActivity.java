3d23293338466318db158960dc532d66e0b42a34
Abdulateef Oladele <oladelea@vcu.edu>
2019-01-30 15:12:53 -0500

    Created Profile and Logout Menu, Created profile page and Profile data page to replace homescreen after registration activity

--- app/src/main/java/com/example/learncode/HomeActivity.java
+++ app/src/main/java/com/example/learncode/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.learncode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId())  {
+            case R.id.menuLogout:
+                Toast.makeText(this, "Logging user out", Toast.LENGTH_SHORT).show();
+                finish();
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+            case R.id.menuProfile:
+                Toast.makeText(this, "Opening user profile", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, ProfileActivity.class));
+                finish();
+                break;
+        }
+        return true;
+
     }
 }
